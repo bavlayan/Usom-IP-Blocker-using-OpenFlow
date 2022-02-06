4714a4b1a3055250e24d2b58969219b2c3ce1c72
bavlayan <b.avlayan@gmail.com>
2022-02-06 18:33:09 +0300

    add testtopo function

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+from subprocess import call
+
 from UsomUrlHelper import UsomUrlHelper
-import socket
-import threading
+from MyToplogy import MyToplogy
+
+#-------------------- Mininet packages --------------------
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.cli import CLI
+#-------------------- Mininet packages --------------------
 
 def create_blocked_url_list():
     usomurlhelper = UsomUrlHelper()
     usomurlhelper.get_blocked_urls_from_usom()
     usomurlhelper.set_ip()
 
+def topo_test():
+    topo = MyToplogy(4)
+    net = Mininet(topo)
+    net.start()
+    print("Dumping host connections")
+    dumpNodeConnections(net.hosts)
+    print("Testing network connectivity")
+    net.pingAll()
+    h1 = net.get('h1')
+    print( "Host", h1.name, "has IP address", h1.IP(), "and MAC address", h1.MAC() )
+    net.stop()
+
 if __name__ == "__main__":
-    create_blocked_url_list()
+    call(["mn", "-c"])
+
+    setLogLevel('info')
+    topo_test()
 
     
